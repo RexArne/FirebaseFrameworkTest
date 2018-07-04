35e4272daaca6b62f4f742b2a902d874282efb64
Arne Koning <arnekoning@gmail.com>
2018-07-04 21:06:18 +0200

    first commit

---
+++ testFramework/MYAFirebaseFramework.framework/Headers/MYAFirebaseFramework.h
@@ -0,0 +1,22 @@
+//
+//  MYAFirebaseFrameworkSwift.h
+//  FirebaseFrameworkSwift
+//
+//  Created by MINDYOURAPP on 27/05/2018.
+//  Copyright © 2018 MINDYOURAPP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseFrameworkSwift.
+FOUNDATION_EXPORT double FirebaseSwiftVersionNumber;
+
+//! Project version string for FirebaseFrameworkSwift.
+FOUNDATION_EXPORT const unsigned char FirebaseSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MYAFirebaseFramework/PublicHeader.h>
+
+
+#import <MYAFirebaseFramework/Firebase.h>
+
+
